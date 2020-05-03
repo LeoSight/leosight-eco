501ef220fc64fd71eb0c96973d6548727b2c13e5
RatajVaver <rataj@fabulasoft.cz>
2020-05-03 20:33:42 +0200

    bugfix: Discord oznámení by se nyní nemělo opakovat při mikrovýpadcích sítě

--- discord.js
+++ discord.js
@@ -3,11 +3,15 @@ console.log('Spouštím Discord integraci..');
 const utils = require(__dirname + '/utils.js')();
 const Discord = require('discord.js');
 const discordClient = new Discord.Client();
+let loaded = false;
 
 discordClient.on("ready", () => {
-    discordClient.user.setActivity("LeoSight Eco", { type: "PLAYING"});
-    discordClient.channels.get("661926637453180958").setTopic(`Diskuze a vývoj herního projektu: https://eco.leosight.cz/ | Poslední aktualizace: ${utils.date()} | Aktuální verze: ${utils.version} | GitHub: https://github.com/${process.env.CODEBASE}`);
-    broadcast('Server je online! https://eco.leosight.cz');
+    if(!loaded) {
+        discordClient.user.setActivity("LeoSight Eco", {type: "PLAYING"});
+        discordClient.channels.get("661926637453180958").setTopic(`Diskuze a vývoj herního projektu: https://eco.leosight.cz/ | Poslední aktualizace: ${utils.date()} | Aktuální verze: ${utils.version} | GitHub: https://github.com/${process.env.CODEBASE}`);
+        broadcast('Server je online! https://eco.leosight.cz');
+        loaded = true;
+    }
 });
 
 discordClient.login(process.env.DISCORD_TOKEN);
